fa7a197e04a6dbca73280c52b7d49a331b4bcf08
robbyhecht <robbyhecht@gmail.com>
2018-10-12 18:21:33 -0500

    Completed battle of the bands challenge

---
+++ battle-of-the-bands.js
@@ -0,0 +1,21 @@
+
+let bandNumber = 0
+const takeNumber = function (bandName) {
+  bandNumber += 1;
+  console.log(`${bandNumber}. ${bandName}`);
+    /*
+        Write your awesome code here. See comments
+        below for what should be returned.
+    */
+}
+
+
+// Your job is to write a function that accepts any band name as an argument. The function will increment a global variable by one each time it is invoked, and return that number, and the band name concatenated together.
+
+
+
+// const scum = takeNumber("Galactic Scum")
+// console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+// const under = takeNumber("Underdogs")
+// console.log(under)  // This should print "2. Underdogs" in the console
--- chickenmonkey.js
+++ chickenmonkey.js
@@ -15,7 +15,7 @@ function chickenmonkey() {
   }
 }
 
-chickenmonkey()
+// chickenmonkey()
 
 
 // Write a program that prints the numbers from 1 to 100. But for multiples of five (5, 10, 15, etc.) print "Chicken" instead of the number and for the multiples of seven (7, 14, 21, etc.) print "Monkey". For numbers which are multiples of both five and seven print "ChickenMonkey".
